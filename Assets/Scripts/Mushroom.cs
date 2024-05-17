7e81800476f1d8b49f71fece690aa49d8b843068
chl1195 <160118949+chl1195@users.noreply.github.com>
2024-05-17 15:16:01 +0900

    [fix] 버섯 수치 조정
    떨어지는 위치 수정

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Mushroom : MonoBehaviour
 {
+    float size = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
         float x = Random.Range(-2.95f, 2.8f);
-        float y = Random.Range(3.0f, 5.0f);
+        float y = (6);
 
         transform.position = new Vector3(x, y, 0);
+
+        int type = Random.Range(1, 4);
+
+        if (type == 1)
+        {
+            size = 0.8f;
+        }
+        else if (type == 2)
+        {
+            size = 1.0f;
+        }
+        else if (type == 3)
+        {
+            size = 1.2f;
+        }
+
+        transform.localScale = new Vector3(size, size, 0);
     }
 
     // Update is called once per frame
