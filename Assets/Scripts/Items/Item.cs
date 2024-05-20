b52f22bfc4876328df0fd70fedd828001d4725c3
JNUSYJ <lukai7501@naver.com>
2024-05-20 15:43:43 +0900

    Fix : 아이템 주석 추가

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -5,15 +5,18 @@ public abstract class Item : MonoBehaviour
 {
     private void Start()
     {
+        // 아이템 효과를 이벤트에 추가
         ItemManager.Instance.ItemEvent += ItemEffect;
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        // 땅에 접촉 시 파괴
         if (collision.gameObject.CompareTag("Ground"))
         {
             Destroy(gameObject);
         }
+        // 플레이어와 접촉 시 아이템 효과를 발동시키고 파괴
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Invincible"))
         {
             ItemManager.Instance.UseItem(collision.gameObject);
@@ -21,5 +24,6 @@ public abstract class Item : MonoBehaviour
         }
     }
 
+    // 아이템 효과를 자식 클래스에서 구현
     public abstract void ItemEffect(GameObject player);
 }
--- Assets/Scripts/Managers/ItemManager.cs
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -14,11 +14,14 @@ public class ItemManager : MonoBehaviour
             Instance = this;
     }
 
+    // 이벤트에 등록된 아이템 효과 사용
     public void UseItem(GameObject player)
     {
         ItemEvent?.Invoke(player);
+        ItemEvent = null;
     }
 
+    // 무적 아이템 후처리
     public void InvincibleItemAfterEffect(GameObject player)
     {
         StartCoroutine("ResetInvincibility", player);
@@ -26,6 +29,7 @@ public class ItemManager : MonoBehaviour
 
     public IEnumerator ResetInvincibility(GameObject player)
     {
+        // 무적 상태 5초 유지 후 원상복귀
         yield return new WaitForSeconds(5f);
         player.tag = "Player";
         player.GetComponentInChildren<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
