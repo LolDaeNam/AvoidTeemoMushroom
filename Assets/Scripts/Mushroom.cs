f2c4b112271e3202a09554eadce86d4b82d9ab49
chl1195 <160118949+chl1195@users.noreply.github.com>
2024-05-20 13:09:22 +0900

    Feat : 버섯 색상과 떨어지는 속도, 위치 추가 및 수정
    버섯 4, 5번 색상 빨강, 파랑으로 바꾸고
    떨어지는 속도는 3.0로 통일하고 2, 5번은 8.0, 5.0으로 조정
    떨어지는 위치는 5번을 0으로 바꾸고 싶은데 안 바껴서 수정중

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -7,31 +7,52 @@ public class Mushroom : MonoBehaviour
 {
     float size = 1.0f;
     int score = 0;
+    Color color = Color.white;
 
-    // 크기와 점수를 배열로 저장
-    float[] sizes = { 2.0f, 1.0f, 5.0f };
-    int[] scores = { 10, 20, 30 };
+    // 크기, 점수, 색상, 떨어지는 속도를 배열로 저장
+    float[] sizes = { 2.0f, 1.0f, 5.0f, 3.0f, 3.0f};
+    int[] scores = { 10, 20, 30, 40, 50 };
+    Color[] colors = { Color.white, Color.white, Color.white, Color.red, Color.blue };
+    float[] Speeds = { 3.0f, 8.0f, 3.0f, 3.0f, 5.0f };
+    // 5번 버섯 y 위치 0으로 설정
+    float[] yPosition = { 5.5f, 5.5f, 5.5f, 5.5f, 0f };
 
-    // Start is called before the first frame update
     void Start()
     {
         float x = Random.Range(-4.95f, 4.95f);
-        float y = (5.5f);
 
-        transform.position = new Vector3(x, y, 0);
+        transform.position = new Vector3(x, 5.5f, 0);
 
-        int type = Random.Range(1, 4);
+        int type = Random.Range(1, 6);
         // 배열 인덱스는 0부터 시작하므로 type 값에서 1을 뺀다.
         int index = type - 1;
 
-        // 배열에서 크기와 점수를 가져온다.
+        // 배열에서 크기, 점수, 색상을 가져온다.
         size = sizes[index];
         score = scores[index];
+        color = colors[index];
+        float Speed = Speeds[index];
+        // 5번 버섯은 y 위치를 배열에서 가져온다.
+        float y = yPosition[index];
 
         transform.localScale = new Vector3(size, size, 0);
+
+        // 색상을 적용
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            // 인스턴스화된 머티리얼을 사용하여 색상 설정
+            renderer.material.color = color;
+        }
+
+        // 떨어지는 속도를 적용
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = new Vector2(0, -Speed);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
