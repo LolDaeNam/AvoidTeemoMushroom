197b190ae639af132c0db269b195f77ebc23bacf
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-05-21 19:53:45 +0900

    Update : E스킬 애니메이션 추가

--- Assets/Scripts/Skills/Eskill.cs
+++ Assets/Scripts/Skills/Eskill.cs
@@ -3,29 +3,42 @@ using UnityEngine;
 
 public class Eskill : AbstractSkill
 {
+    PlayerAnimationContorller playerAnimation;
     public PlayerHealthSystem healthSystem;
     public SprtieChangeController spriteChangeController;
+    public GameObject Sword;
+
+    private void Start()
+    {
+        
+    }
 
     public override void Activate(GameObject player)
     {
         healthSystem = player.GetComponent<PlayerHealthSystem>();
+        playerAnimation = player.GetComponent<PlayerAnimationContorller>();
         if (healthSystem != null)
         {
             healthSystem.damageReduction = 0f;
         }
         spriteChangeController.SpriteChange(1);
         GameManager.Instance.isActiveSkill = true;
+        Sword.SetActive(true);
+        playerAnimation.OnSkillHit();
         
     }
 
     public override void Deactivate(GameObject player)
     {
         healthSystem = player.GetComponent<PlayerHealthSystem>();
+        playerAnimation = player.GetComponent<PlayerAnimationContorller>();
         if (healthSystem != null)
         {
             healthSystem.damageReduction = 1f;
         }
         spriteChangeController.SpriteChange(0);
         GameManager.Instance.isActiveSkill = false;
+        Sword.SetActive(false);
+        playerAnimation.OutSkillHit();
     }
 }
